a0b4b5304aaa2deb916b74a2e072f2dfd5a56f39
tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>
2023-05-30 22:14:36 +0300

    chore: initial
    Signed-off-by: tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>

---
+++ main.ts
@@ -0,0 +1,16 @@
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+
+import "$std/dotenv/load.ts";
+
+import { start } from "$fresh/server.ts";
+import manifest from "./fresh.gen.ts";
+
+import twindPlugin from "$fresh/plugins/twindv1.ts";
+import twindConfig from "./twind.config.ts";
+
+// deno-lint-ignore no-explicit-any
+await start(manifest, { plugins: [twindPlugin(twindConfig as any)] });
